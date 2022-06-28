4be4a5b4cc77c04c8bea9e33b8ffb2aacedcb6a4
Zane McCaig <zanemccaig@gmail.com>
2022-06-28 13:01:05 -0700

    Finalized the test

--- gmail.test.js
+++ gmail.test.js
@@ -1,6 +1,7 @@
 
 const googleApis = require('googleapis').google;
 const _ = require('lodash');
+require('dotenv').config();
 
 const { GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, GMAIL_ACCESS_TOKEN, GMAIL_REFRESH_TOKEN } = process.env;
 
